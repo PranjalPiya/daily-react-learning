2ce382ccb9bd2757a89fd95389288e8d38bfa2de
PranjalPiya <pranjalpiya10@gmail.com>
2024-08-29 20:42:25 +0545

    inital commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Day1 from './day/Day1';
+
+function App() {
+  return (
+    <div>
+      <Day1></Day1>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/day/Day1.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+
+//creating a list of static data to fetch in the UI.
+const staticData = [
+  'Apple',
+  'Mango',
+  'Orange',
+  'Grapes',
+];
+
+const staticDataTwo = [{
+  'Name': 'Pranjal Piya',
+  'Address': 'Chitwan',
+  'Age': '23',
+  'Image': 'https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+},
+{
+  'Name': 'Siddhartha Tamang',
+  'Address': 'Kathmandu',
+  'Age': '24',
+  'Image': 'https://images.pexels.com/photos/8173265/pexels-photo-8173265.jpeg'
+},
+{
+  'Name': 'Maniraj Gurung',
+  'Address': 'Palpa',
+  'Age': '26',
+  'Image': 'https://images.pexels.com/photos/2589653/pexels-photo-2589653.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+},
+{
+  'Name': 'Niraj Shrestha',
+  'Address': 'Dolpa',
+  'Age': '33',
+  'Image': 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+},
+
+]
+
+const Day1 = () => {
+  return (
+    <div>
+      <h1>Displaying List of static Data</h1>
+      {/* Mapping the list of object.*/}
+      <ul>{staticData.map((e) => {
+        return <li><p>{e}</p></li>
+      })}</ul>
+
+      <h1>Displaying list with different objects and values</h1>
+
+    </div>
+  )
+}
+
+export default Day1
