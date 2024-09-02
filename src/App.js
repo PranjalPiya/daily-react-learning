c80cea0b801321a17159517d8f6dbaaa496a2ea9
PranjalPiya <pranjalpiya10@gmail.com>
2024-09-02 19:15:17 +0545

    Learn: using fetch method to fetch data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Day1 from './day/Day1';
+import Day2 from './day/Day2';
 
 function App() {
   return (
     <div>
-      <Day1></Day1>
+      {/* <Day1></Day1> */}
+      <Day2></Day2>
     </div>
 
   );
---
+++ src/day/Day2.jsx
@@ -0,0 +1,30 @@
+
+//WE WILL LEARN HOW TO FETCH DATA FROM THE API USING DIFFERENT METHODS.
+
+// FREE API :- "https://dummyjson.com/products/"
+
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+//we will use fetch() method first
+
+const Day2 = () => {
+    const [item, setItem] = useState([]);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/photos')
+            .then((res) => {
+                return res.json();
+            }).then((data) => {
+                console.log(data);
+                setItem(data);
+            });
+    }, []);
+    return (<div>
+        {item.map((data) => {
+            return <p key={data.id}>{data.title}</p>
+        })}
+    </div>)
+
+}
+
+export default Day2
